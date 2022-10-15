e359506409e8762bca8afa0c93feb3393ce7aa02
Alex <onegunnerunder@gmail.com>
2022-10-15 13:52:26 -0700

    Added Coin class. New class inherits from the CollectibleItem abstract class.

---
+++ CollectibleShopManager/Coin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CollectibleShopManager
+{
+    internal class Coin : CollectibleItem
+    {
+        public int Year { get; set; }
+
+        public Coin() { }
+
+        public Coin(int year, string name, string desc, decimal cost, decimal 
+            sellPrice) : base(name, desc, cost, sellPrice)
+        {
+
+        }
+    }
+}
