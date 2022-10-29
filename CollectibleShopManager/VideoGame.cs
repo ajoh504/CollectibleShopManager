aac605b340298214db7512cfb170873ec4c0202d
ajoh504 <onegunnerunder@gmail.com>
2022-10-29 14:38:58 -0700

    style: reordered class properties

--- CollectibleShopManager/VideoGame.cs
+++ CollectibleShopManager/VideoGame.cs
@@ -2,18 +2,17 @@
 {
     /// <summary>
     /// Defines a Video Game inventory item. This class inherits from the Inventory base class.
-    /// It contains one unique property: Platform, i.e. Sega Dreamcast, PlayStation, etc.
     /// </summary>
     internal class VideoGame : Inventory
     {
-        public string? Platform { get; private set; }
         public string? Title { get; private set; }
+        public string? Platform { get; private set; }
         public VideoGame() { }
         public VideoGame(string? title, string? platform, string? partNumber, int upc, string? desc, decimal cost, decimal sellPrice) 
             : base(partNumber, upc, desc, cost, sellPrice) // Inherited from Inventory class
         {
-            Platform = platform;
             Title = title;
+            Platform = platform;
         }
     }
 }
