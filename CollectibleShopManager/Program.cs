d56aee1a7bad11a80b821f3f8d26bd4283d40f17
ajoh504 <onegunnerunder@gmail.com>
2022-10-30 14:51:26 -0700

    refactor: add ability to get object properties
    Create the PrintObjectProperties() method. This method allows for the retrieval of any object properties. This same logic can be used to simplify the menu class by creating a reusable menu. The new menu will print only the properties of the given object, allowing the user to specify the data in order to build a new object (of any inventory type)

--- CollectibleShopManager/Program.cs
+++ CollectibleShopManager/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CollectibleShopManager
+﻿using System.Reflection;
+
+namespace CollectibleShopManager
 {
     internal class Program
     {
@@ -16,6 +18,7 @@ Please select one of the following:");
 
                 Console.Write(@"
 Video Game Menu Screen .... 1 
+TEST: get object properties 2
 Quit to Desktop ........... Q
 ");
 
@@ -26,7 +29,28 @@ Quit to Desktop ........... Q
                     VideoGameMenu videoGameMenu = new VideoGameMenu();
                     videoGameMenu.Execute();
                 }
+                else if(mainChoice == "2") /// TEST: get object properties
+                {
+                    string homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                    JsonConfig jsonConfig = new JsonConfig();
+                    List<VideoGame> gamesList = jsonConfig.GetDeserializedList($"{homeDirectory}\\videogames.json");
+                    foreach (var game in gamesList)
+                    {
+                        PrintObjectProperties(game);
+                    }
+                    Console.ReadLine();
+                }
                 else if (mainChoice.ToUpper() == "Q") Environment.Exit(0);
+            }
+
+            static void PrintObjectProperties(Object obj)
+            {
+                Type t = obj.GetType();
+                PropertyInfo[] gameProperties = t.GetProperties();
+                foreach (var prop in gameProperties)
+                {
+                    Console.WriteLine(prop.GetValue(obj));
+                }
             }
         }
     }
