ffb293b10b2f557eccbd23e88264a0c7b52eb402
ajoh504 <onegunnerunder@gmail.com>
2022-11-01 06:26:23 -0700

    feat: move code logic to Inventory class
    Inventory class contains two new methods: GetObjectProperties(), which returns an array of PropertyInfo objects; and GetPropertyValues(), which takes an array of PropertyInfo objects as an argument, and returns an array of an object's values.

--- CollectibleShopManager/Inventory.cs
+++ CollectibleShopManager/Inventory.cs
@@ -1,4 +1,6 @@
-﻿namespace CollectibleShopManager
+﻿using System.Reflection;
+
+namespace CollectibleShopManager
 {
     /// <summary>
     /// Defines a generic inventory item with no special properties. 
@@ -24,6 +26,34 @@
         {
             return false;
         }
+
+        /// <summary>
+        /// Given an object as an argument, get an array of its properties as PropertyInfo 
+        /// objects.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns> An array of PropertyInfo[] objects. </returns>
+        public PropertyInfo[] GetObjectProperties(Object obj)
+        {
+            Type t = obj.GetType();
+            PropertyInfo[] properties = t.GetProperties();
+            return properties;
+        }
+
+        /// <summary>
+        /// Given an array of PropertyInfo objects, return an array that contains the values of 
+        /// each property.
+        /// </summary>
+        /// <param name="properties"></param>
+        /// <returns> Object[] containing all property values. </returns>
+        public Object[] GetPropertyValues(PropertyInfo[] properties)
+        {
+            Object[] propertyValues = new object[properties.Length];
+            for (int i = 0; i < propertyValues.Length; i++)
+            {
+                propertyValues[i] = properties[i].GetValue();
+            }
+        } 
 
         public Inventory() { }
         public Inventory(string partNumber, int upc, string desc, decimal cost, decimal sellPrice)
