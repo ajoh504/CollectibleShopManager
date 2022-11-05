cc13b46d292090ad90517e1110eafafcde9b9341
ajoh504 <onegunnerunder@gmail.com>
2022-11-05 07:29:49 -0700

    refactor: remove test code

--- CollectibleShopManager/Program.cs
+++ CollectibleShopManager/Program.cs
@@ -18,7 +18,6 @@ Please select one of the following:");
 
                 Console.Write(@"
 Video Game Menu Screen .... 1 
-TEST: get object properties 2
 Quit to Desktop ........... Q
 ");
 
@@ -28,25 +27,6 @@ Quit to Desktop ........... Q
                 {
                     MenuSelectionScreen videoGameMenu = new MenuSelectionScreen();
                     videoGameMenu.Execute();
-                }
-                else if(mainChoice == "2") /// TEST: get object properties
-                {
-                    string homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-                    JsonFileConfiguration jsonConfig = new JsonFileConfiguration();
-                    List<VideoGame> gamesList = jsonConfig.GetDeserializedList($"{homeDirectory}\\videogames.json");
-
-                    foreach (var game in gamesList)
-                    {
-                        PropertyInfo[] videoGameProperties = game.GetPropertyInfo();
-                        Object[] videoGamePropertyValues = game.GetPropertyValues();
-
-                        for (int i = 0; i < videoGameProperties.Length; i++)
-                        {
-                            Console.WriteLine($"{videoGameProperties[i].Name}: {videoGamePropertyValues[i]}");
-                        }
-                        Console.WriteLine("\n");
-                    }
-                    Console.ReadLine();
                 }
                 else if (mainChoice.ToUpper() == "Q") Environment.Exit(0);
             }
