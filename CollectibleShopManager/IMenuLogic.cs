16809d58735fa09c5153d2d3dc36b8f93e7095ce
ajoh504 <onegunnerunder@gmail.com>
2022-11-19 09:01:20 -0800

    refactor: add IMenuLogic interface

---
+++ CollectibleShopManager/IMenuLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectibleShopManager
+{
+    public interface IMenuLogic
+    {
+        List<string> GetAllMenuQuestions();
+        void SetMenuValues(List<string> menuQuestions);
+    }
+}
