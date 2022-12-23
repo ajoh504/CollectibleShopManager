86a182e1a594477bf01da45c9cdbf952473828a4
Alex <onegunnerunder@gmail.com>
2022-12-23 17:12:14 -0600

    refactor: add Console class

---
+++ GameGrubber/InventoryItems/Console.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GameGrubber.InventoryItems
+{
+    public class Console : Inventory
+    {
+        private string? regionCode;
+        private string? modelNumber;
+        private string? brandName;
+        private string? color;
+        private bool? hasCustomTheme;
+
+        public string? RegionCode
+        {
+            get { return regionCode; }
+            set { regionCode = value; }
+        }
+
+        public string? ModelNumber
+        {
+            get { return modelNumber; }
+            set { modelNumber = value; }
+        }
+
+        public string BrandName 
+        { 
+            get { return brandName; }
+            set { brandName = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public bool HasCustomTheme
+        {
+            get { return hasCustomTheme; }
+            set { hasCustomTheme= value; }
+        }
+    }
+}
+
