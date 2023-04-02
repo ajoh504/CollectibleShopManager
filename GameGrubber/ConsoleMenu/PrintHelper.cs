9252d6df6aeea0f789f1ecd3abc9c022bf6ab373
ajoh504 <onegunnerunder@gmail.com>
2023-04-02 12:53:48 -0700

    refactor: comment out JSON references - must refactor to read from the database

--- GameGrubber/ConsoleMenu/PrintHelper.cs
+++ GameGrubber/ConsoleMenu/PrintHelper.cs
@@ -1,10 +1,4 @@
 ﻿using GameGrubber.InventoryItems;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameGrubber.ConsoleMenu
 {
@@ -14,40 +8,38 @@ namespace GameGrubber.ConsoleMenu
         /// Prints a single T inventory object to the console as specified by the user.
         /// </summary>
         /// <param name="ID"> Numeric ID to view a single inventory item. </param>
-        public static void PrintSingleInventoryObject(int ID, ref JsonFile<T> jsonFile)
+        public static void PrintSingleInventoryObject(int ID)
         {
-            List<T> jsonList = jsonFile.GetDeserializedList();
-
-            foreach (var inventoryObject in jsonList)
-            {
-                if (inventoryObject.InventoryID == ID)
-                {
-                    PropertyInfo[] inventoryPropertyInfo = inventoryObject.GetPropertyInfo();
-                    object[] inventoryPropertyValues = inventoryObject.GetPropertyValues();
-                    for (int i = 0; i < inventoryPropertyValues.Length; i++)
-                    {
-                        Console.WriteLine($"{inventoryPropertyInfo[i].Name}: {inventoryPropertyValues[i]}");
-                    }
-                }
-            }
+            //foreach (var inventoryObject in jsonList)
+            //{
+            //    if (inventoryObject.InventoryID == ID)
+            //    {
+            //        PropertyInfo[] inventoryPropertyInfo = inventoryObject.GetPropertyInfo();
+            //        object[] inventoryPropertyValues = inventoryObject.GetPropertyValues();
+            //        for (int i = 0; i < inventoryPropertyValues.Length; i++)
+            //        {
+            //            Console.WriteLine($"{inventoryPropertyInfo[i].Name}: {inventoryPropertyValues[i]}");
+            //        }
+            //    }
+            //}
         }
 
         /// <summary>
         /// Print all T inventory objects and their properties to the console.
         /// </summary>
-        public static void PrintAllInventoryObjects(ref JsonFile<T> jsonFile)
+        public static void PrintAllInventoryObjects()
         {
-            List<T> jsonList = jsonFile.GetDeserializedList();
+            //List<T> jsonList = jsonFile.GetDeserializedList();
 
-            foreach (var inventoryObject in jsonList)
-            {
-                PropertyInfo[] inventoryPropertyInfo = inventoryObject.GetPropertyInfo();
-                object[] inventoryPropertyValues = inventoryObject.GetPropertyValues();
-                for (int i = 0; i < inventoryPropertyValues.Length; i++)
-                {
-                    Console.WriteLine($"{inventoryPropertyInfo[i].Name}: {inventoryPropertyValues[i]}");
-                }
-            }
+            //foreach (var inventoryObject in jsonList)
+            //{
+            //    PropertyInfo[] inventoryPropertyInfo = inventoryObject.GetPropertyInfo();
+            //    object[] inventoryPropertyValues = inventoryObject.GetPropertyValues();
+            //    for (int i = 0; i < inventoryPropertyValues.Length; i++)
+            //    {
+            //        Console.WriteLine($"{inventoryPropertyInfo[i].Name}: {inventoryPropertyValues[i]}");
+            //    }
+            //}
         }
     }
 }
