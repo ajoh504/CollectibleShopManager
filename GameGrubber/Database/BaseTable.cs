b88ad01a9d1f9f96de01fb618d4e2b39b2f83bba
ajoh504 <onegunnerunder@gmail.com>
2023-04-02 09:12:38 -0700

    refactor: make BaseTable abstract - add Insert methods to insert data into the database

--- GameGrubber/Database/BaseTable.cs
+++ GameGrubber/Database/BaseTable.cs
@@ -2,7 +2,7 @@
 
 namespace GameGrubber.Database
 {
-    internal class BaseTable
+    internal abstract class BaseTable
     {
         /// <summary>
         /// Return an opened database connection object
@@ -13,5 +13,51 @@ namespace GameGrubber.Database
             connection.Open();
             return connection;
         }
+
+        /// <summary>
+        /// Insert a string value into the specified table and column
+        /// </summary>
+        protected void Insert(string table, string column, string value) 
+        {
+            using(SQLiteConnection connection = GetConnection())
+            {
+                string cmd = $"INSERT INTO {table} ({column}) values ({value})";
+                using(SQLiteCommand command = new SQLiteCommand(cmd, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Insert an integer value into the specified table and column
+        /// </summary>
+        protected void Insert(string table, string column, int value)
+        {
+            using (SQLiteConnection connection = GetConnection())
+            {
+                string cmd = $"INSERT INTO {table} ({column}) values ({value})";
+                using (SQLiteCommand command = new SQLiteCommand(cmd, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Insert an decimal value into the specified table and column
+        /// </summary>
+        protected void Insert(string table, string column, decimal value)
+        {
+            using (SQLiteConnection connection = GetConnection())
+            {
+                string cmd = $"INSERT INTO {table} ({column}) values ({value})";
+                using (SQLiteCommand command = new SQLiteCommand(cmd, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
     }
 }
