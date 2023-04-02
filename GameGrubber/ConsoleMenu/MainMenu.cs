c2850a001289ff309bed5c87d7eb006b8e764f0e
ajoh504 <onegunnerunder@gmail.com>
2023-04-02 08:05:04 -0700

    refactor: move Main Menu logic to a separate class - Program.cs will be responsible for checking program files at startup - Program.cs invokes MainMenu.Execute() to start the program

---
+++ GameGrubber/ConsoleMenu/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿namespace GameGrubber.ConsoleMenu
+{
+    internal static class MainMenu
+    {
+        public static void Execute()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(@"
+____________________________________
+
+Game Grubber - Main Menu
+____________________________________");
+
+                Console.Write(@"
+Point of Sale ................... 1
+Inventory ....................... 2
+Quit to Desktop ................. Q
+");
+
+                string choice = Console.ReadLine();
+                if (choice == "1") PointOfSaleSelectionMenu.Execute();
+                else if (choice == "2") InventorySelectionMenu.Execute();
+                else if (choice.ToUpper() == "Q") Environment.Exit(0);
+            }
+        }
+    }
+}
--- GameGrubber/Program.cs
+++ GameGrubber/Program.cs
@@ -1,12 +1,13 @@
-﻿using System.Reflection;
+﻿using System.Data.SQLite;
 using GameGrubber.ConsoleMenu;
-using GameGrubber.InventoryItems;
 
 namespace GameGrubber
 {
     internal class Program
     {
         public static string mainDir = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\GameGrubber";
+        public static string databasePath = $"{mainDir}\\ggdb.sqlite";
+
         /// <summary>
         /// Defines the main menu execution flow
         /// </summary>
@@ -15,26 +16,12 @@ namespace GameGrubber
             // Create the main program directory in the user's home directory.
             Directory.CreateDirectory(mainDir);
 
-            while (true)
+            if (!File.Exists(databasePath))
             {
-                Console.Clear();
-                Console.WriteLine(@"
-____________________________________
-
-Game Grubber - Main Menu
-____________________________________");
-
-                Console.Write(@"
-Point of Sale ................... 1
-Inventory ....................... 2
-Quit to Desktop ................. Q
-");
-
-                string choice = Console.ReadLine();
-                if (choice == "1") PointOfSaleSelectionMenu.Execute(); 
-                else if (choice == "2") InventorySelectionMenu.Execute();
-                else if (choice.ToUpper() == "Q") Environment.Exit(0);
+                SQLiteConnection.CreateFile(databasePath);
             }
+            
+            MainMenu.Execute();
         }
     }
-}
+}     
