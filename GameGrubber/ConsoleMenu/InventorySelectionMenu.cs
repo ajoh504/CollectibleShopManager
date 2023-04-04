513580fded89805e9ffb9e72f688610eef1fabd2
ajoh504 <onegunnerunder@gmail.com>
2023-04-04 05:55:43 -0700

    refactor: add logic to handle all inventory types

--- GameGrubber/ConsoleMenu/InventorySelectionMenu.cs
+++ GameGrubber/ConsoleMenu/InventorySelectionMenu.cs
@@ -17,24 +17,52 @@ Please select one of the following:
 ____________________________________________________");
 
                 Console.Write(@"
-Standard Item Menu Screen ....... 1
-Video Game Menu Screen .......... 2
-Go Back ......................... B
-Quit to Desktop ................. Q
+Generic item menu screen ........ 1
+Video game menu screen .......... 2
+Gaming console menu screen ...... 3
+Strategy guide menu screen ...... 4
+Controller menu screen .......... 5
+Accessory menu screen ........... 6
+Go back ......................... B
+Quit to desktop ................. Q
 ");
 
                 string mainMenuChoice = Console.ReadLine();
                 if (mainMenuChoice == "1")
                 {
                     Inventory inventory = new Inventory();
-                    InventoryEditorMenu<Inventory> inventoryMenu = new InventoryEditorMenu<Inventory>(ref inventory, "Standard Item");
+                    InventoryEditorMenu<Inventory> inventoryMenu = new InventoryEditorMenu<Inventory>(ref inventory, "generic item");
                     inventoryMenu.Execute();
                 }
                 else if (mainMenuChoice == "2")
                 {
                     VideoGame videoGame = new VideoGame();
-                    InventoryEditorMenu<VideoGame> videoGameMenu = new InventoryEditorMenu<VideoGame>(ref videoGame, "Video Game");
+                    InventoryEditorMenu<VideoGame> videoGameMenu = new InventoryEditorMenu<VideoGame>(ref videoGame, "video game");
                     videoGameMenu.Execute();
+                }
+                else if (mainMenuChoice == "3")
+                {
+                    VideoGameConsole gamingConsole = new VideoGameConsole();
+                    InventoryEditorMenu<VideoGameConsole> gamingConsoleMenu = new InventoryEditorMenu<VideoGameConsole>(ref gamingConsole, "gaming console");
+                    gamingConsoleMenu.Execute();
+                }
+                else if (mainMenuChoice == "4")
+                {
+                    StrategyGuide strategyGuide = new StrategyGuide();
+                    InventoryEditorMenu<StrategyGuide> strategyGuideMenu = new InventoryEditorMenu<StrategyGuide>(ref strategyGuide, "strategy guide");
+                    strategyGuideMenu.Execute();
+                }
+                else if (mainMenuChoice == "5")
+                {
+                    Controller controller= new Controller();
+                    InventoryEditorMenu<Controller> controllerMenu = new InventoryEditorMenu<Controller>(ref controller, "controller");
+                    controllerMenu.Execute();
+                }
+                else if (mainMenuChoice == "6")
+                {
+                    Accessory accessory= new Accessory();
+                    InventoryEditorMenu<Accessory> accessoryMenu = new InventoryEditorMenu<Accessory>(ref accessory, "accessory");
+                    accessoryMenu.Execute();
                 }
                 else if (mainMenuChoice.ToUpper() == "B") return;
                 else if (mainMenuChoice.ToUpper() == "Q") Environment.Exit(0);
