70dbaaa095256372707c2a1f8bd8009ed5cc8662
ajoh504 <onegunnerunder@gmail.com>
2023-04-04 05:56:46 -0700

    refactor: add logic to print the values from a single inventory row

--- GameGrubber/Database/BaseTable.cs
+++ GameGrubber/Database/BaseTable.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Specialized;
 using System.Data.SQLite;
+using System.Dynamic;
 using System.Text;
 
 namespace GameGrubber.Database
@@ -104,9 +105,13 @@ namespace GameGrubber.Database
             }
         }
 
-        public List<string> SelectAll(string table)
+        /// <summary>
+        /// Get a string list of all row values from the specified table, separated by commas
+        /// </summary>
+        /// <param name="table"> Table to select values from </param>
+        /// <returns> A string list of all row values </returns>
+        protected List<string> SelectAll(string table)
         {
-            List<object> objects = new List<object>();
             List<string> values = new List<string>();
             using (SQLiteConnection connection = GetConnection())
             {
@@ -124,18 +129,46 @@ namespace GameGrubber.Database
                                 sb.Append(rowValues[key]);
                                 sb.Append(", ");
                             }
-                            sb.Remove(sb.Length - 1, 1);
-                            objects.Add(sb.ToString());
+                            sb.Remove(sb.Length - 1, 1); // remove final comma
+                            values.Add(sb.ToString());
                         }
                     }
                 }
             }
-
-            foreach (object item in objects)
-            {
-                values.Add((string)item);
-            }
             return values;
+        }
+
+        /// <summary>
+        /// Get a string of all row values from the specified database column
+        /// </summary>
+        /// <param name="table"> Table to select from </param>
+        /// <param name="id"> id of the row to select from </param>
+        /// <returns> A string of all row values, separated by commas </returns>
+        protected string SelectSingleRow(string table, int id)
+        {
+            using (SQLiteConnection connection = GetConnection())
+            {
+                string cmd = $"SELECT * FROM {table} WHERE inventory_id = {id}";
+                using (SQLiteCommand command = new SQLiteCommand(cmd, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader()) 
+                    {
+                        while (reader.Read())
+                        {
+                            NameValueCollection rowValues = reader.GetValues();
+                            StringBuilder sb = new StringBuilder();
+                            foreach (string key in rowValues.AllKeys)
+                            {
+                                sb.Append(rowValues[key]);
+                                sb.Append(", ");
+                            }
+                            sb.Remove(sb.Length - 1, 1); // remove final comma
+                            return sb.ToString();
+                        }
+                    }
+                }
+            }
+            return "";
         }
     }
 }
