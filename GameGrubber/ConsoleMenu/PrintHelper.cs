9b1e265174a93f9c54c71e3d6e58b15fe016bd22
ajoh504 <onegunnerunder@gmail.com>
2023-04-04 05:56:15 -0700

    refactor: update methods to call inventory printer methods - new methods will read from the database

--- GameGrubber/ConsoleMenu/PrintHelper.cs
+++ GameGrubber/ConsoleMenu/PrintHelper.cs
@@ -8,38 +8,17 @@ namespace GameGrubber.ConsoleMenu
         /// Prints a single T inventory object to the console as specified by the user.
         /// </summary>
         /// <param name="ID"> Numeric ID to view a single inventory item. </param>
-        public static void PrintSingleInventoryObject(int ID)
+        public static void PrintSingleInventoryObject(ref T inventoryObject, int id)
         {
-            //foreach (var inventoryObject in jsonList)
-            //{
-            //    if (inventoryObject.InventoryID == ID)
-            //    {
-            //        PropertyInfo[] inventoryPropertyInfo = inventoryObject.GetPropertyInfo();
-            //        object[] inventoryPropertyValues = inventoryObject.GetPropertyValues();
-            //        for (int i = 0; i < inventoryPropertyValues.Length; i++)
-            //        {
-            //            Console.WriteLine($"{inventoryPropertyInfo[i].Name}: {inventoryPropertyValues[i]}");
-            //        }
-            //    }
-            //}
+            inventoryObject.PrintSingleRow(id);
         }
 
         /// <summary>
         /// Print all T inventory objects and their properties to the console.
         /// </summary>
-        public static void PrintAllInventoryObjects()
+        public static void PrintAllInventoryObjects(ref T inventoryObject)
         {
-            //List<T> jsonList = jsonFile.GetDeserializedList();
-
-            //foreach (var inventoryObject in jsonList)
-            //{
-            //    PropertyInfo[] inventoryPropertyInfo = inventoryObject.GetPropertyInfo();
-            //    object[] inventoryPropertyValues = inventoryObject.GetPropertyValues();
-            //    for (int i = 0; i < inventoryPropertyValues.Length; i++)
-            //    {
-            //        Console.WriteLine($"{inventoryPropertyInfo[i].Name}: {inventoryPropertyValues[i]}");
-            //    }
-            //}
+            inventoryObject.PrintAllData();
         }
     }
 }
