dbde1056b7bbc52847e285a4f70d9b994f6c58d0
ajoh504 <onegunnerunder@gmail.com>
2023-04-05 06:00:20 -0700

    refactor: add invoice class - database will store an id, a date, a price, and the items sold

---
+++ GameGrubber/Items/Invoice.cs
@@ -0,0 +1,16 @@
+﻿namespace GameGrubber.Items
+{
+    internal class Invoice
+    {
+        private int invoiceId;
+        private string tableName;
+        private DateTime date;
+        private decimal price;
+        private List<string> itemsSold;
+
+        public Invoice()
+        {
+            this.tableName = "invoice";
+        }
+    }
+}
