ffa9f79a7a1355250832eb6223478b84aa7a5d3b
ajoh504 <onegunnerunder@gmail.com>
2023-04-06 05:51:55 -0700

    refactor: add logic for console menu flow - The main selection is to create an invoice. If this is selected, instantiate an Invoice object

--- GameGrubber/ConsoleMenu/PointOfSaleSelectionMenu.cs
+++ GameGrubber/ConsoleMenu/PointOfSaleSelectionMenu.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GameGrubber.Items;
 
 namespace GameGrubber.ConsoleMenu
 {
@@ -10,7 +6,31 @@ namespace GameGrubber.ConsoleMenu
     {
         public static void Execute()
         {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(@"
+____________________________________________________
 
+Point of Sale Selection Menu
+Please select one of the following:
+____________________________________________________");
+
+                Console.Write(@"
+Make a sale ..................... 1
+Go back ......................... B
+Quit to desktop ................. Q
+");
+
+                string posMenuChoice = Console.ReadLine();
+                if (posMenuChoice == "1")
+                {
+                    Invoice invoice = new Invoice();
+                    invoice.AddNewRow();
+                }
+                else if (posMenuChoice.ToUpper() == "B") return;
+                else if (posMenuChoice.ToUpper() == "Q") Environment.Exit(0);
+            }
         }
     }
 }
