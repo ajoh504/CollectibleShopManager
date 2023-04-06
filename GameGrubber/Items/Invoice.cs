20e540c6d2bb1142e6b77ff82abada2b5ac9ed01
ajoh504 <onegunnerunder@gmail.com>
2023-04-06 05:55:57 -0700

    refactor: add logic to create new database row - update private fields to store database column names

--- GameGrubber/Items/Invoice.cs
+++ GameGrubber/Items/Invoice.cs
@@ -1,16 +1,44 @@
-﻿namespace GameGrubber.Items
+﻿using GameGrubber.Database;
+using GameGrubber.InventoryItems;
+
+namespace GameGrubber.Items
 {
-    internal class Invoice
+    internal class Invoice : BaseTable
+
     {
         private int invoiceId;
         private string tableName;
         private DateTime date;
         private decimal price;
         private List<string> itemsSold;
+        private const string dateColumn = "date";
+        private const string priceColumn = "price";
+        private const string itemsSoldColumn = "items_sold";
 
         public Invoice()
         {
             this.tableName = "invoice";
         }
+
+        /// <summary>
+        /// Add a new row to the database column
+        /// </summary>
+        public void AddNewRow()
+        {
+            string row = SelectSingleRow(tableName, invoiceId);
+            if (row != "")
+            {
+                string[] _ = row.Split(",");
+                string stringId = _[0];
+                int currentId = Int32.Parse(stringId);
+                if (currentId == invoiceId)
+                {
+                    invoiceId = GetNextAvailableID(tableName);
+                    NewRow(tableName, invoiceId);
+                    return;
+                }
+            }
+            NewRow(tableName, invoiceId);
+        }   
     }
 }
