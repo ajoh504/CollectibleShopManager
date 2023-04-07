929cfdcd96233fe2e4d506648bc3c8713308823b
ajoh504 <onegunnerunder@gmail.com>
2023-04-07 05:31:57 -0700

    refactor: add class to search for database values - This class will determine if a specified value(s) exist in the database

---
+++ GameGrubber/Database/DatabaseValueSearch.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SQLite;
+
+namespace GameGrubber.Database
+{
+    internal class DatabaseValueChecker
+    {
+        /// <summary>
+        /// Return an opened database connection object
+        /// </summary>
+        private SQLiteConnection GetConnection()
+        {
+            SQLiteConnection connection = new SQLiteConnection($"Data Source={Program.databasePath};Version=3;");
+            connection.Open();
+            return connection;
+        }
+
+        /// <summary>
+        /// Return true if the value exists in the database, false otherwise
+        /// </summary>
+        public bool Exists(string table, string column, string value)
+        {
+            using (SQLiteConnection connection = GetConnection())
+            {
+
+            }
+            return false;
+        }
+    }
+}
