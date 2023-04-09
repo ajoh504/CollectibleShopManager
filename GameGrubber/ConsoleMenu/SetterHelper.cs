ced102e36b2f5f5e58adda3e7aa73b94ed11de57
ajoh504 <onegunnerunder@gmail.com>
2023-04-09 08:09:05 -0700

    refactor: add logic to verify item code - check to see if item code exists in the database. - If the item code exists, inform the user to try again. - if the item code entered is an empty string, inform the user to try again

--- GameGrubber/ConsoleMenu/SetterHelper.cs
+++ GameGrubber/ConsoleMenu/SetterHelper.cs
@@ -46,9 +46,16 @@ namespace GameGrubber.ConsoleMenu
 
             foreach (var prop in stringProperties)
             {
-                Console.WriteLine($"Add: {prop.Name} for a new {item} or press enter to skip");
-                string value = Console.ReadLine();
-                prop.SetValue(inventoryObject, value, null);
+                if (prop.Name.Equals("ItemCode"))
+                {
+                    VerifyItemCode(item, ref inventoryObject);
+                }
+                else
+                {
+                    Console.WriteLine($"Add: {prop.Name} for a new {item} or press enter to skip");
+                    string value = Console.ReadLine();
+                    prop.SetValue(inventoryObject, value, null);
+                }
             }
         }
 
@@ -72,6 +79,33 @@ namespace GameGrubber.ConsoleMenu
                 {
                     Console.WriteLine("Value set to 0");
                     prop.SetValue(inventoryObject, 0M, null);
+                }
+            }
+        }
+
+        private static void VerifyItemCode(string item, ref T inventoryObject)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Add: A unique ItemCode for a new {item} (required)");
+                string itemCode = Console.ReadLine();
+                if(itemCode == "")
+                {
+                    Console.WriteLine("ItemCode is required. Press any key to try again.");
+                    Console.ReadLine();
+                    continue;
+                }
+                else if (inventoryObject.ItemCodeExists(itemCode))
+                {
+                    Console.WriteLine($@"
+{itemCode} is already associated with an inventory item.
+Press any key to enter a new item code.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    inventoryObject.ItemCode = itemCode;
+                    return;
                 }
             }
         }
