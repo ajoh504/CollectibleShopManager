b69fce48f1b62a876d9cc000815d6afd9e1c35cb
ajoh504 <onegunnerunder@gmail.com>
2023-04-15 08:06:54 -0700

    refactor: create new class for checkout menu logic - This class is responsible for allowing the user to create a sale

---
+++ GameGrubber/ConsoleMenu/CheckoutMenu.cs
@@ -0,0 +1,40 @@
+﻿using GameGrubber.Database;
+using GameGrubber.Items;
+
+namespace GameGrubber.ConsoleMenu
+{
+    internal class CheckoutMenu
+    {
+        private Invoice invoice;
+        private DatabaseValueSearch databaseValueSearch;
+        private List<string> itemsToSell;
+
+        public CheckoutMenu() 
+        { 
+            invoice = new Invoice();
+            databaseValueSearch = new DatabaseValueSearch();
+            itemsToSell = new List<string>();   
+        }
+
+        public void Execute()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write(@"
+Go back ........................... B
+Add an item to this sale by entering 
+the item code:
+");
+
+                string item = Console.ReadLine();
+                if (item.ToUpper().Equals("B")) return;
+                if (databaseValueSearch.ItemCodeExists(item)) 
+                {
+                    itemsToSell.Add(item);
+                } 
+                else Console.Write("Item code not found. Press enter to continue.");
+            }
+        }
+    }
+}
