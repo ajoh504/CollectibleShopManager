f1dd05d16689dee6477c4d8092111f4787393781
ajoh504 <onegunnerunder@gmail.com>
2023-04-15 08:50:38 -0700

    refactor: remove UpdateRow method overloads, and add generic type parameter instead

--- GameGrubber/Database/DatabaseNonQuery.cs
+++ GameGrubber/Database/DatabaseNonQuery.cs
@@ -34,42 +34,12 @@ namespace GameGrubber.Database
         /// <summary>
         /// Insert a string value into the specified table and column
         /// </summary>
-        public void UpdateRow(string table, string column, string value, int id) 
+        public void UpdateRow<T>(string table, string column, T value, int id) 
         {
             using(SQLiteConnection connection = GetConnection())
             {
                 string cmd = $"UPDATE {table} SET {column} = '{value}' WHERE id = {id}";
                 using(SQLiteCommand command = new SQLiteCommand(cmd, connection))
-                {
-                    command.ExecuteNonQuery();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Insert an integer value into the specified table and column
-        /// </summary>
-        public void UpdateRow(string table, string column, int value, int id)
-        {
-            using (SQLiteConnection connection = GetConnection())
-            {
-                string cmd = $"UPDATE {table} SET {column} = {value} WHERE id = {id}";
-                using (SQLiteCommand command = new SQLiteCommand(cmd, connection))
-                {
-                    command.ExecuteNonQuery();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Insert a decimal value into the specified table and column
-        /// </summary>
-        public void UpdateRow(string table, string column, decimal value, int id)
-        {
-            using (SQLiteConnection connection = GetConnection())
-            {
-                string cmd = $"UPDATE {table} SET {column} = {value} WHERE id = {id}";
-                using (SQLiteCommand command = new SQLiteCommand(cmd, connection))
                 {
                     command.ExecuteNonQuery();
                 }
