5386bd75c8eedb6ed7c2079b11ceadbe979691d8
ajoh504 <onegunnerunder@gmail.com>
2023-04-15 08:07:11 -0700

    refactor: move checkout screen logic to its own class

--- GameGrubber/ConsoleMenu/PointOfSaleSelectionMenu.cs
+++ GameGrubber/ConsoleMenu/PointOfSaleSelectionMenu.cs
@@ -1,7 +1,4 @@
-﻿using GameGrubber.InventoryItems;
-using GameGrubber.Items;
-
-namespace GameGrubber.ConsoleMenu
+﻿namespace GameGrubber.ConsoleMenu
 {
     internal class PointOfSaleSelectionMenu
     {
@@ -26,18 +23,8 @@ Quit to desktop ................. Q
                 string posMenuChoice = Console.ReadLine();
                 if (posMenuChoice == "1")
                 {
-                    Invoice invoice = new Invoice();
-                    Inventory inventoryObject = new Inventory();
-                    List<string> itemsToSell = new List<string>();
-                    invoice.AddNewRow();
-
-                    while (true)
-                    {
-                        Console.WriteLine("Enter a part number for the sale:\n");
-                        string item = Console.ReadLine();
-                        // if values exists in db, add to invoice. else inform user
-                        itemsToSell.Add(item);
-                    }
+                    CheckoutMenu checkoutMenu = new CheckoutMenu();
+                    checkoutMenu.Execute();
                 }
                 else if (posMenuChoice.ToUpper() == "B") return;
                 else if (posMenuChoice.ToUpper() == "Q") Environment.Exit(0);
