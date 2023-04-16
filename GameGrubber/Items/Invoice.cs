90459fb411d3e9c6510aecb28a7a8749a0ec7361
ajoh504 <onegunnerunder@gmail.com>
2023-04-16 09:04:22 -0700

    refactor: move Subtotal logic to Invoice class

--- GameGrubber/Items/Invoice.cs
+++ GameGrubber/Items/Invoice.cs
@@ -3,12 +3,13 @@
 namespace GameGrubber.Items
 {
     internal class Invoice
-
     {
         private int invoiceId;
         private string tableName;
         private DateTime date;
         private decimal price;
+        private int subTotal;
+        private List<string> itemsToSell;
         private List<string> itemsSold;
         private const string dateColumn = "date";
         private const string priceColumn = "price";
@@ -34,10 +35,17 @@ namespace GameGrubber.Items
             set { itemsSold = value; }
         }
 
+        public int SubTotal
+        {
+            get { return subTotal; }
+            private set { value = CalculateSubtotal(); }
+        }
+
         public Invoice()
         {
             this.tableName = "invoice";
             itemsSold = new List<string>();
+            itemsToSell = new List<string>();
             nonQuery = new DatabaseNonQuery();
             valueSearch = new DatabaseValueSearch();
         }
@@ -54,11 +62,35 @@ namespace GameGrubber.Items
         }
 
         /// <summary>
+        /// Add an item to sell to update the subtotal
+        /// </summary>
+        public void AddItemToSell(string item)
+        {
+            itemsToSell.Add(item);
+            subTotal = CalculateSubtotal();
+        }
+
+        /// <summary>
         /// Add a comma delimiter to the items sold on this invoice, and convert the list to a 
         /// string value
         /// </summary>
         /// <param name="items"> String List to format </param>
         /// <returns> Formatted string to store in the database </returns>
         private string Format(List<string> items) => String.Join(',', items);
+
+        private int CalculateSubtotal()
+        {
+            int[] totals = new int[itemsToSell.Count];
+            foreach (string item in itemsToSell)
+            {
+                valueSearch.SelectSingleRow()
+            }
+            return 0;
+        }
+
+        private int GetPrice(string itemCode)
+        {
+
+        }
     }
 }
